0a41a7891c8d724665d70522acec338b042524ca
박정민 <pjm14418@gmail.com>
2024-10-28 18:04:34 +0900

    feat: 학습 과정 논리 흐름도 작성
    #3

---
+++ train.py
@@ -0,0 +1,24 @@
+# 필요한 라이브러리 import
+
+
+# 하이퍼 파라미터 정의 (epoch, lr, etc...)
+
+# 데이터 load (Dataset)
+## 데이터 전처리
+
+# 데이터 로더 정의 (DataLoader)
+
+# 모델 선언
+
+# loss, optimizer 정의
+
+# (for) 데이터 불러오기
+# 불러온 데이터 모델에 넣기
+
+# 출력을 바탕으로 loss 계산
+# loss로 back propagation
+# optimizer로 최적화 진행
+
+
+# 학습 중간 결과로 성능을 평가
+# 성능이 만족스럽다면 weight 저장
