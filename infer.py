6bff003efda0de6dfd8852560ea40e5eb82cc09a
박정민 <pjm14418@gmail.com>
2024-10-29 16:55:17 +0900

    feat: 추론 과정 데이터 준비
    #4

--- infer.py
+++ infer.py
@@ -1,6 +1,25 @@
 # 패키지를 임포트
+import torch
+from PIL import Image
+from torchvision import transforms
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # 추론에 사용할 데이터를 준비 (전처리)
+image_path = "test_image.jpeg"
+image = Image.open(image_path)
+image = image.resize((32, 32))
+
+CIFAR_MEAN = [0.491, 0.482, 0.447]
+CIFAR_STD = [0.247, 0.244, 0.262]
+
+transform = transforms.Compose[
+    transforms.ToTensor(), transforms.Normalize(mean=CIFAR_MEAN, std=CIFAR_STD)
+]
+
+image = transform(image)
+image = image.unsqueeze(0).to(device)
 
 ## 학습이 완료된 최고의 모델을 준비하기
 # 설계도 + Hparam 모델 껍대기 만들기
